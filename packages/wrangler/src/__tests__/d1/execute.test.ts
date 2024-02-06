e9df3b1d3b8445b92789a75e45bccec5e83b6446
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2024-02-06 17:53:10 +0000

    add test for --persist-to and --remote

--- packages/wrangler/src/__tests__/d1/execute.test.ts
+++ packages/wrangler/src/__tests__/d1/execute.test.ts
@@ -89,4 +89,18 @@ describe("execute", () => {
 			runWrangler("d1 migrations apply --local db --preview")
 		).rejects.toThrowError(`Error: can't use --preview with --local`);
 	});
+
+	it("should reject --remote when using --persist-to", async () => {
+		setIsTTY(false);
+		writeWranglerToml({
+			d1_databases: [
+				{ binding: "DATABASE", database_name: "db", database_id: "xxxx" },
+			],
+		});
+		await runWrangler("d1 migrations create db test");
+
+		await expect(
+			runWrangler("d1 migrations apply db --remote --persist-to asdf")
+		).rejects.toThrowError(`persist-to -> local`);
+	});
 });
